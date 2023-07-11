1e8da3fb056083249a85f1cf65e0e4995bcb1955
rhtld123 <rhtld123@gmail.com>
2023-07-11 23:30:25 +0900

    initial commit

---
+++ src/main/java/com/study/battleqconfig/BattleqConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.study.battleqconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleqConfigserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BattleqConfigserverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/study/battleqconfig/BattleqConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.battleqconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleqConfigserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
